7e51468528f363fc7f745236db81bf9ca6cbb5b1
Juan Sebastian Goldberg <sebas.goldberg@gmail.com>
2018-10-09 20:16:56 -0300

    Test of correct behavior of ComponentContainer propagateModel attribute.

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,14 @@
 import Controller from "sap/ui/core/mvc/Controller";
 import formatter from "iamsoft/test/component/reuse/model/formatter";
+import JSONModel from "sap/ui/model/json/JSONModel";
 
 export default Controller.extend("iamsoft.test.component.reuse.controller.App", {
 
     formatter: formatter,
 
     onInit: function () {
-
+        let v = this.getView();
+        v.setModel(new JSONModel({buttonText: "Texto do Botão"}));
+        v.byId("page").bindObject("/");
     }
 });
